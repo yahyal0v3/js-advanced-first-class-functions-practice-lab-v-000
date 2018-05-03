df2055c67d42df7259db3c0081007552cab81095
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 03:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  drivers.forEach(driver => console.log(driver.name))
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(drivers) {
+  const driversCopy = [...drivers]
+  return driversCopy.sort(function(a, b) {
+    return a.revenue - b.revenue
+  })
+}
+
+function driversByName(drivers) {
+  const driversCopy = [...drivers]
+  return driversCopy.sort(function(a, b) {
+    return a.name.localeCompare(b.name)
+  })
+}
+
+function totalRevenue(drivers) {
+  const driversCopy = [...drivers]
+  return driversCopy.reduce((total, driver) => total + driver.revenue)
+}
